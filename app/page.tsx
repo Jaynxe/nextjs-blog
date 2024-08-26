33c39795839a5778e087866dc5d1cdb370e546d3
xjy <2742665196@qq.com>
2024-08-26 11:28:28 +0800

    Fine-tuned the layout and added date display

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import { allPosts } from "@/.contentlayer/generated"
 import Link from "next/link"
+import { formatDate } from '@/app/utils'
 
 export default function Home() {
   return (
-    <div className="prose dark:prose-invert">
+    <div className="prose dark:prose-invert mx-auto pb-10">
       {allPosts.map((post) => (
-        <article key={post._id}>
+        <article key={post._id} className="mb-8">
           <Link href={post.slug}>
-            <h2>{post.title}</h2>
+            <h2 className="text-2xl font-bold mb-2">{post.title}</h2>
           </Link>
-          {post.description && <p>{post.description}</p>}
+          {post.description && <p className="mb-4">{post.description}</p>}
+          <p className="text-sm text-gray-500 dark:text-gray-400">
+            {formatDate(post.date)}
+          </p>
         </article>
       ))}
     </div>
--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -3,6 +3,7 @@ import { allPosts } from "contentlayer/generated"
 
 import { Metadata } from "next"
 import { Mdx } from "@/components/mdx-components"
+import { formatDate } from '@/app/utils'
 
 interface PostProps {
   params: {
@@ -51,12 +52,15 @@ export default async function PostPage({ params }: PostProps) {
 
   return (
     <article className="py-6 prose dark:prose-invert">
-      <h1 className="mb-2">{post.title}</h1>
+      <h1 className="mb-2.5">{post.title}</h1>
       {post.description && (
-        <p className="text-xl mt-0 text-slate-700 dark:text-slate-200">
+        <p className="text-xl my-0 text-slate-700 dark:text-slate-200">
           {post.description}
         </p>
       )}
+      <p className="text-sm text-gray-500 dark:text-gray-400">
+        {formatDate(post.date)}
+      </p>
       <hr className="my-4" />
       <Mdx code={post.body.code} />
     </article>
