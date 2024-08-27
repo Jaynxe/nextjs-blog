a3b4f7b66e833b279f0b0c4ae91816d09210aa5f
xjy <2742665196@qq.com>
2024-08-27 10:42:49 +0800

    Fixed: Selected text when switching themes

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -1,26 +1,29 @@
 "use client"
 
 import { useTheme } from "next-themes"
-import { useState, useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme()
-  const [isDark, setIsDark] = useState(false)
+  const [mounted, setMounted] = useState(false)
 
   useEffect(() => {
-    setIsDark(theme === "dark")
-  }, [theme])
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
 
   return (
     <div
-      className={`w-12 h-6 flex items-center bg-gray-300 dark:bg-gray-700 rounded-full cursor-pointer transition-all duration-200`}
-      onClick={() => setTheme(isDark ? "light" : "dark")}
+      className={`w-12 h-6 flex items-center bg-gray-300 dark:bg-gray-700 rounded-full cursor-pointer transition-all duration-300`}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      style={{ userSelect: 'none' }}
     >
       <div
-        className={`w-5 h-5 bg-white dark:bg-black rounded-full flex items-center justify-center transition-transform duration-200 transform ${isDark ? "translate-x-6" : "translate-x-1"
+        className={`w-5 h-5 bg-white dark:bg-black rounded-full flex items-center justify-center transition-transform duration-300 transform ${theme === "dark" ? "translate-x-6" : "translate-x-1"
           }`}
       >
-        {isDark ? (
+        {theme === "dark" ? (
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -48,7 +51,6 @@ export function ModeToggle() {
               d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
             />
           </svg>
-
         )}
       </div>
     </div>
